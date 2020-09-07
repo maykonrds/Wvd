10b91068ee34a69bf195abe3e02a02528374b38a
Maykon <maykon.santos@valtech.com>
2020-09-07 15:59:13 -0300

    #.

---
+++ Add-VM.ps1
@@ -0,0 +1,16 @@
+# Get Registration Token
+$Subs=Get-AzSubscription | Out-GridView -Title "Select the Subscription" -PassThru
+Set-AzContext -Subscription $Subs
+
+$Rg='rg-wvd'
+$HostPoolName=(Get-AzWvdHostPool).Name | Out-GridView -Title "Select the HostPool Name" -PassThru
+
+
+if(!(Get-AzWvdRegistrationInfo -ResourceGroupName $Rg -HostPoolName $HostPoolName).Token)
+{
+    Write-Host "You must create a Registration" -ForegroundColor Red
+    $RegistrationToken=New-AzWvdRegistrationInfo -ResourceGroupName $Rg -HostPoolName $HostPoolName -ExpirationTime $((get-date).ToUniversalTime().AddDays(10).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))
+}
+
+Write-Host $RegistrationToken.Token -ForegroundColor Green
+
